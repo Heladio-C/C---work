307f2bc45f40ddfcbb340b69d9933e493168ccb5
irregular-user <heladio522@gmail.com>
2026-06-12 13:01:19 -0700

    Initial commit with clean workspace and gitignore

---
+++ 23.1/BankAccount.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "BankAccount.h"
+using namespace std;
+
+
+BankAccount::BankAccount(string newName, double chBalance, double sBalance) : customerName(newName), checkingBalance(chBalance), savingBalance(sBalance){}
+
+//setters 
+void BankAccount::SetName(string newName){
+   customerName = newName;
+}
+
+void BankAccount::SetChkBalance(double balance){
+   checkingBalance = balance;
+}
+
+void BankAccount::SetSavBalance(double balance){
+   savingBalance = balance;
+}
+
+//getters 
+
+string BankAccount::GetName() const {
+   return customerName;
+}
+
+double BankAccount::GetChkBalance() const {
+   return checkingBalance;
+}
+
+double BankAccount::GetSavBalance() const {
+   return savingBalance;
+}
+
+//functions
+
+void BankAccount::DepositChecking(double amt){
+   if(amt > 0.0){
+      checkingBalance += amt;
+   }
+}
+
+void BankAccount::DepositSavings(double amt){
+   if(amt > 0.0){
+      savingBalance += amt;
+   }
+}
+
+void BankAccount::WithdrawChecking(double amt){
+   if(amt > 0.0){
+      checkingBalance -= amt;
+   }
+}
+
+void BankAccount::WithdrawSavings(double amt){
+   if(amt > 0.0){
+      savingBalance -= amt;
+   }
+}
+
+void BankAccount::TransferToChecking(double amt){
+   if(amt > 0.0){
+      savingBalance -= amt;
+      checkingBalance += amt;
+   }
+}
+
+
+
+MyBankAccount::MyBankAccount(string name, double chkBalance, double savBalance, double fee) : BankAccount(name, chkBalance, savBalance), transactionFee(fee) {}
+
+void MyBankAccount::WithdrawSavings(double amt){
+   double totalAmt = amt + transactionFee;
+
+   if(totalAmt <= GetSavBalance()){
+      SetSavBalance(GetSavBalance() - totalAmt);
+   }
+}
+
+void MyBankAccount::TransferToChecking(double amt){
+   double totalAmt = amt + transactionFee;
+   if(totalAmt <= GetSavBalance()){
+      SetSavBalance(GetSavBalance() - totalAmt);
+      SetChkBalance(GetChkBalance() + amt);
+   }
+}
