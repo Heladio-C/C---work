90f6316755b1bf06239bf1d7d93e3c9d5d5933e0
irregular-user <heladio522@gmail.com>
2026-06-12 13:35:58 -0700

    Implement Tower of Hanoi solution with moveStack and moveDisk functions

---
+++ topic_4/toh/main.cpp
@@ -0,0 +1,8 @@
+#include "toh.h"
+
+int main (int argc, char const *argv[])
+{
+
+    moveStack(3, 'A', 'C', 'B');
+    return 0;
+}
---
+++ topic_4/toh/toh.h
@@ -0,0 +1,10 @@
+#include <cstddef>
+#ifndef TOH_H
+#define TOH_H
+
+
+void moveDisk(size_t, char, char);
+void moveStack(size_t, char, char, char);   
+
+#endif // TOH_H
+
